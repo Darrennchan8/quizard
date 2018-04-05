d1fcd5411308e090582e76f66baa5716be49b32f
Darren Chan <darrennchan8@Gmail.com>
2018-04-04 21:34:59 -0400

    Remove unused imports and whitespace; use AppCompatActivity instead of FragmentActivity to preserve AppBar.

--- app/src/main/java/com/example/darren/quizard/CreateQuizActivity.java
+++ app/src/main/java/com/example/darren/quizard/CreateQuizActivity.java
@@ -1,17 +1,12 @@
 package com.example.darren.quizard;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.view.inputmethod.InputMethodManager;
 
-public class CreateQuizActivity extends FragmentActivity {
+public class CreateQuizActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
