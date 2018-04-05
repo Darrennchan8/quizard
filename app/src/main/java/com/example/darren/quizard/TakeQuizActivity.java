93e9f0eb7c900d2e6fd9f7dbd395b1b91f1684eb
Patrick Miller <millerwp@vcu.edu>
2018-04-05 00:31:50 -0400

    Create mockQuiz JSON object from JSON string Pull in quiz name from mockQuiz to display on take_quiz_activity

--- app/src/main/java/com/example/darren/quizard/TakeQuizActivity.java
+++ app/src/main/java/com/example/darren/quizard/TakeQuizActivity.java
@@ -4,8 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class TakeQuizActivity extends AppCompatActivity {
+
+    TextView quizName;
+    Button beginQuiz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +24,24 @@ public class TakeQuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_take_quiz);
     }
 
-    public void beginQuiz(View v) {
+    @Override
+    protected void onStart(){
+        super.onStart();
+        JSONObject mockQuiz = null;
+        try {
+            mockQuiz = new JSONObject(getString(R.string.mock_quiz));
+            quizName = findViewById(R.id.quiz_name);
+            String name = mockQuiz.getJSONObject("quiz").getString("quiz_name");
+            quizName.setText(name);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Log.e(getClass().getName(), "Could not read JSON", e);
+            throw new RuntimeException(e);
+        }
+        beginQuiz = (Button) findViewById(R.id.begin_quiz);
+    }
 
+    public void beginQuiz(View v) {
         Intent i;
         i = new Intent(TakeQuizActivity.this, QuestionView.class);
         startActivity(i);
