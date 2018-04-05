1b0908a97dc76cca69274f96d12d4f7c68d7e2ad
Amy Brick <brickaj@vcu.edu>
2018-04-04 20:50:50 -0400

    Amy's changes  -- take quiz activity  -- question view

--- app/src/main/java/com/example/darren/quizard/TakeQuizActivity.java
+++ app/src/main/java/com/example/darren/quizard/TakeQuizActivity.java
@@ -1,7 +1,9 @@
 package com.example.darren.quizard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TakeQuizActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class TakeQuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_quiz);
+    }
+
+    public void beginQuiz(View v) {
+
+        Intent i;
+        i = new Intent(TakeQuizActivity.this, QuestionView.class);
+        startActivity(i);
     }
 }
