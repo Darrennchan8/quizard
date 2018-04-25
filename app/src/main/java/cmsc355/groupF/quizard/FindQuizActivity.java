1846b9e451595ad2c3fd86a5890abc307bc659e8
Amy Brick <brickaj@vcu.edu>
2018-04-24 23:01:57 -0400

    FindQuizActivity and layout mostly complete, minus button functionality

--- app/src/main/java/cmsc355/groupF/quizard/FindQuizActivity.java
+++ app/src/main/java/cmsc355/groupF/quizard/FindQuizActivity.java
@@ -1,10 +1,18 @@
 package cmsc355.groupF.quizard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseError;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import cmsc355.groupF.quizard.quiz.Quiz;
@@ -16,22 +24,56 @@ import cmsc355.groupF.quizard.quiz.QuizUtils;
 //3. button
 // firebase
 
+/*
+TO FIX:
+-- make button do something
+   -- go to next page if valid quiz
+   -- (for invalid quiz name) display error message OR be disabled until valid quiz is entered
+-- no new line in text box
+ */
+
+
 public class FindQuizActivity extends AppCompatActivity {
+
+    ArrayAdapter<String> adapter;
+    AutoCompleteTextView textView;
+    ArrayList<String> QUIZZES = new ArrayList<String>();
+    Button searchQuizBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_quiz);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, QUIZZES);
+        textView = findViewById(R.id.autoCompleteTextView);
+        textView.setAdapter(adapter);
+        textView.setThreshold(0);
+
+
         QuizUtils.getAllQuizzes(new QuizUtils.QuizQueryCallback() {
             @Override
             public void onLoad(List<Quiz> quizzes) {
-                int i = 0;
+                ArrayList<String> temp = new ArrayList<>(quizzes.size());
+                for(int i=0; i<quizzes.size(); i++) {
+                    temp.add(quizzes.get(i).getTitle());
+                }
+                QUIZZES.clear();
+                QUIZZES.addAll(temp);
+                adapter.notifyDataSetChanged();
             }
 
             @Override
             public void onError(DatabaseError err) {
-
             }
         });
     }
-}
+
+    public void onClickSearch(View view) {
+        //This is just to test the toast
+        //Will be changed later (see notes at top)
+        Toast.makeText(getApplicationContext(), "Quiz not found", Toast.LENGTH_SHORT).show();
+    }
+
+
+    }
+
