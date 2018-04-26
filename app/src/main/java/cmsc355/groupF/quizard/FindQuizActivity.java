4bf1fb87ee77c4c855a7915ebb7ee0b940edb0ba
Amy Brick <brickaj@vcu.edu>
2018-04-26 18:54:52 -0400

    Made AutoCompleteTextView clickable, added hint and padding, removed button

--- app/src/main/java/cmsc355/groupF/quizard/FindQuizActivity.java
+++ app/src/main/java/cmsc355/groupF/quizard/FindQuizActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
@@ -12,42 +13,26 @@ import android.widget.Toast;
 import com.google.firebase.database.DatabaseError;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import cmsc355.groupF.quizard.quiz.Quiz;
 import cmsc355.groupF.quizard.quiz.QuizUtils;
 
-//User will be able to search through list of available quizzes to select the correct one
-//1. text box
-//2. drop-down, auto updating list
-//3. button
-// firebase
-
-/*
-TO FIX:
--- make button do something
-   -- go to next page if valid quiz
-   -- (for invalid quiz name) display error message OR be disabled until valid quiz is entered
--- no new line in text box
- */
-
 
 public class FindQuizActivity extends AppCompatActivity {
 
     ArrayAdapter<String> adapter;
-    AutoCompleteTextView textView;
+    AutoCompleteTextView autoCompleteTextView;
     ArrayList<String> QUIZZES = new ArrayList<String>();
-    Button searchQuizBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_quiz);
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, QUIZZES);
-        textView = findViewById(R.id.autoCompleteTextView);
-        textView.setAdapter(adapter);
-        textView.setThreshold(0);
+        autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
+        autoCompleteTextView.setAdapter(adapter);
+        autoCompleteTextView.setThreshold(0);
 
 
         QuizUtils.getAllQuizzes(new QuizUtils.QuizQueryCallback() {
@@ -66,7 +51,17 @@ public class FindQuizActivity extends AppCompatActivity {
             public void onError(DatabaseError err) {
             }
         });
+
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(FindQuizActivity.this, TakeQuizActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 
     public void onClickSearch(View view) {
         //This is just to test the toast
